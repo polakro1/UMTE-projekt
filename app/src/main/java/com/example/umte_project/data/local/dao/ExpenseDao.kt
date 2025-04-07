11b9c89404a0f53977be34a3a0e80bda642c1ea1
Robin Polák <polakro1@uhk.cz>
2025-04-07 21:53:55 +0200

    Add ExpenseDao
    - Adds `ExpenseDao` interface with methods for inserting, deleting, retrieving all, and retrieving an expense by ID.
    - Annotates the interface with `@Dao`.
    - Defines methods `insertExpense`, `deleteExpense`, `getAllExpenses`, and `getExpenseById`.

---
+++ app/src/main/java/com/example/umte_project/data/local/dao/ExpenseDao.kt
@@ -0,0 +1,24 @@
+package com.example.umte_project.data.local.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.umte_project.data.local.entities.ExpenseEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface ExpenseDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertExpense(expense: ExpenseEntity)
+
+    @Delete
+    suspend fun deleteExpense(expense: ExpenseEntity)
+
+    @Query("SELECT * FROM expenses order by date DESC")
+    fun getAllExpenses(): Flow<List<ExpenseEntity>>
+
+    @Query("SELECT * FROM expenses WHERE id = :id")
+    suspend fun getExpenseById(id: Long): ExpenseEntity?
+}
