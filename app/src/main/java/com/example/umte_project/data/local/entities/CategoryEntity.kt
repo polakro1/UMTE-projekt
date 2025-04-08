669548ac7a014a98552444c584bb35c5bc5c4341
Robin Polák <polakro1@uhk.cz>
2025-04-08 19:12:14 +0200

    Refactor CategoryEntity and ExpenseEntity
    - Changes `CategoryEntity` and `ExpenseEntity` classes to data classes.
    - Changes the table name of `CategoryEntity` from "Expenses" to "expenses".

--- app/src/main/java/com/example/umte_project/data/local/entities/CategoryEntity.kt
+++ app/src/main/java/com/example/umte_project/data/local/entities/CategoryEntity.kt
@@ -3,8 +3,8 @@ package com.example.umte_project.data.local.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "Expenses")
-class CategoryEntity(
+@Entity(tableName = "expenses")
+data class CategoryEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     val iconRes: Int?, // odkaz na res?
--- app/src/main/java/com/example/umte_project/data/local/entities/ExpenseEntity.kt
+++ app/src/main/java/com/example/umte_project/data/local/entities/ExpenseEntity.kt
@@ -4,7 +4,7 @@ import androidx.room.Entity
 import androidx.room.PrimaryKey
 
 @Entity(tableName = "expenses")
-class ExpenseEntity(
+data class ExpenseEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
     val title: String,
