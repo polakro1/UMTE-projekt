573c03e9e6d80e15e30aec64b13d41c470ebeb3d
Robin Polák <polakro1@uhk.cz>
2025-04-08 23:54:08 +0200

    Update CategoryEntity table name
    - Changes the table name for `CategoryEntity` from "expenses" to "categories".

--- app/src/main/java/com/example/umte_project/data/local/entities/CategoryEntity.kt
+++ app/src/main/java/com/example/umte_project/data/local/entities/CategoryEntity.kt
@@ -3,7 +3,7 @@ package com.example.umte_project.data.local.entities
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "expenses")
+@Entity(tableName = "categories")
 data class CategoryEntity(
     @PrimaryKey(autoGenerate = true)
     val id: Long = 0,
