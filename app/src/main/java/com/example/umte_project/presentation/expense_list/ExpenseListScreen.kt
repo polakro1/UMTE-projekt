b5575ccf2f2f46475b273bf139d25cff9a58ccc7
Robin Polák <polakro1@uhk.cz>
2025-04-10 17:22:33 +0200

    Add ExpenseListScreen
    - Adds `ExpenseListScreen` composable.
    - Adds `ExpenseItem` composable.
    - Uses `LazyColumn` to display a list of expenses.
    - Uses `collectAsState` to observe the expenses flow.
    - Uses `koinViewModel` to inject the `ExpenseListViewModel`.

---
+++ app/src/main/java/com/example/umte_project/presentation/expense_list/ExpenseListScreen.kt
@@ -0,0 +1,28 @@
+package com.example.umte_project.presentation.expense_list
+
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import com.example.umte_project.domain.models.Expense
+import org.koin.androidx.compose.koinViewModel
+
+@Composable
+fun ExpenseListScreen(viewModel: ExpenseListViewModel = koinViewModel()) {
+    val expenses by viewModel.expenses.collectAsState()
+
+    Text("Ahoj")
+    LazyColumn {
+        items(expenses) { expense ->
+            ExpenseItem(expense = expense)
+        }
+    }
+}
+
+@Composable
+fun ExpenseItem(expense: Expense) {
+    Card() { }
+}
