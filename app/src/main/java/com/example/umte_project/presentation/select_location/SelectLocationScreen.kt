adae088aeaa52fd621af7ff27d2b8bb45b389bb8
Robin Polák <polakro1@uhk.cz>
2025-04-25 20:59:06 +0200

    Refactor SelectLocationScreen and ConfirmButton
    - Replaces the IconButton in `SelectLocationScreen` with a `ConfirmButton`.
    - Moves `Map` to be the background in `SelectLocationScreen`.
    - Adds `isLoading` parameter to `ConfirmButton`.
    - Improves layout of `ConfirmButton` to include an icon and the button's text.
    - Adds `ConfirmButton`'s modifier parameter.
    - Makes `enabled` default to `true` in `ConfirmButton`.

--- app/src/main/java/com/example/umte_project/presentation/select_location/SelectLocationScreen.kt
+++ app/src/main/java/com/example/umte_project/presentation/select_location/SelectLocationScreen.kt
@@ -1,19 +1,26 @@
 package com.example.umte_project.presentation.select_location
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Check
+import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.example.umte_project.presentation.ui_components.ConfirmButton
 import com.example.umte_project.presentation.ui_components.Map
 import com.google.android.gms.maps.model.CameraPosition
 import com.google.android.gms.maps.model.LatLng
@@ -32,17 +39,22 @@ fun SelectLocationScreen(
         position = CameraPosition.fromLatLngZoom(selectedPosition, 14f)
     }
 
-    Column {
-        IconButton(onClick = {
-            navController.previousBackStackEntry
-                ?.savedStateHandle
-                ?.set("selected_location", selectedPosition)
-            navController.popBackStack()
-        }) {
-            Icon(Icons.Default.Check, contentDescription = "Confirm")
-        }
-
+    Box(modifier = Modifier.fillMaxSize()) {
         Map(initialLatLng = selectedPosition)
+
+        ConfirmButton(
+            text = "Save location",
+            onClick = {
+                navController.previousBackStackEntry
+                    ?.savedStateHandle
+                    ?.set("selected_location", selectedPosition)
+                navController.popBackStack()
+            },
+            isLoading = false,
+            modifier = Modifier
+                .align(Alignment.BottomCenter)
+                .padding(16.dp)
+        )
     }
 
 }
--- app/src/main/java/com/example/umte_project/presentation/ui_components/ConfirmButton.kt
+++ app/src/main/java/com/example/umte_project/presentation/ui_components/ConfirmButton.kt
@@ -1,5 +1,7 @@
 package com.example.umte_project.presentation.ui_components
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
@@ -9,6 +11,7 @@ import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.focus.focusModifier
 import androidx.compose.ui.graphics.Color
@@ -16,20 +19,32 @@ import androidx.compose.ui.unit.dp
 
 @Composable
 fun ConfirmButton(
+    modifier: Modifier = Modifier,
     text: String,
     onClick: () -> Unit,
     isLoading: Boolean,
-    enabled: Boolean,
-    modifier: Modifier = Modifier
+    enabled: Boolean = true,
 ) {
-    Button(onClick = onClick, enabled = enabled, modifier = modifier) {
+    Button(
+        onClick = onClick,
+        enabled = enabled && !isLoading,
+        modifier = modifier
+    ) {
         if (isLoading) {
             CircularProgressIndicator(
                 color = Color.White, modifier = Modifier.size(20.dp), strokeWidth = 2.dp
             )
         } else {
-            Icon(imageVector = Icons.Default.Done, contentDescription = "save expense")
-            Text(text)
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Icon(
+                    imageVector = Icons.Default.Done,
+                    contentDescription = "Confirm"
+                )
+                Text(text)
+            }
         }
     }
 }
